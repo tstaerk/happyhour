4d5ae6750f41dcc0362b0e3343a80782a9bffc0c
Thorsten Staerk <thorstenstaerk@google.com>
2025-07-14 19:37:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ try:
     # load the API key from streamlit's secrets
     api_key = st.secrets["google_api_key"]
 except (FileNotFoundError, KeyError):
-    
 genai.configure(api_key=api_key)
 
 # Dies ist die gesamte "Persönlichkeit" des Chatbots
